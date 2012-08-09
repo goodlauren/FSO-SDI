efcfb70cb4594481957423cf69bdc34c36dd015c
goodlauren <goodlauren@fullsail.edu>
2012-08-09 20:45:00 -0300

    Update capell_lauren_assignment1.js

--- capell_lauren_assignment1.js
+++ capell_lauren_assignment1.js
@@ -14,13 +14,13 @@ console.log("You will be there, right?", Totally);
 
 if (Totally === true) {
 	if (GirlCount >= BoyCount) {
-		console.log("Every guy will have a girl to hold onto for the scary parts, lol!")
+		console.log("Every guy will have a girl to hold onto for the scary parts, lol!");
 	} else {
 		Invitees = BoyCount - GirlCount;
 		console.log("We need to invite" Invitees," more girls before tonight.");
 	};
-else {
-	if (MovieChoice === "Harry Potter")
+} else {
+	if (MovieChoice === "Harry Potter") {
 		console.log("Great choice of movies; I love this one!");
 	} else {
 		Tease = Nickname + "is obviously too mature to hang out with the likes of", FriendOne,"and us."
